a37ff302ccfe5ca6760cb58b8ffbd1e3b2668208
cipher-tech <nickchibuikem@gmail.com>
2022-08-11 00:39:01 +0100

    Updated test and added type def for SpeechSynthesisUtterance

--- src/hooks/useTextToSpeech.tsx
+++ src/hooks/useTextToSpeech.tsx
@@ -1,22 +1,30 @@
 import { useState, useEffect, useCallback } from "react";
 
 // define our window function types
-
+class NewWindow {
+    text() { }
+}
 
 const useTextToSpeech = (activate: boolean, text: string) => {
-    // const [ text, setOurText ] = useState("go home")
-    const msg = new SpeechSynthesisUtterance()
+    // get the class from the window object
+    let speech = (window as any)[ "SpeechSynthesisUtterance" ] as any
+
+    // if its undefined set it to a custom object to prevent crashing
+    if (typeof speech == "undefined") {
+        speech = NewWindow;
+    }
+    const msg = new speech()
 
     const speechHandler = useCallback((msg: SpeechSynthesisUtterance) => {
         msg.text = text
         window.speechSynthesis.speak(msg)
     },
-        [text],
+        [ text ],
     )
 
 
     useEffect(() => {
-        if (activate) {            
+        if (activate) {
             speechHandler(msg)
         }
     }, [ activate, text ])
